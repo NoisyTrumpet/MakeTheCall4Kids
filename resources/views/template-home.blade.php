48dc5459624404766d5573f39ed3b3c5b5366dbf
CesarBenavides777 <cesarbenavides@icloud.com>
2019-08-19 12:17:49 -0500

    Colors, Fonts, and tentative BG Image done.

--- resources/views/template-home.blade.php
+++ resources/views/template-home.blade.php
@@ -7,6 +7,24 @@
 @section('content')
   @while(have_posts()) @php the_post() @endphp
     @include('partials.page-header')
-    @include('partials.content-page')
+    {{-- @include('partials.content-page') --}}
+
+    <div class="gradContainer">
+    	<h3>Color Test</h3>
+    <p class="textGreen">Green</p>
+    <p class="textLightBlue">Light Blue</p>
+    <p class="textPurple">Purple</p>
+    <p class="textMaroon">Maroon</p>
+    <p class="textOrange">Orange</p>
+    <p class="textRed">Red</p>
+
+    <h3 class="quickSand bold">Font Test</h3>
+    <p class="quickSand light">Light</p>
+    <p class="quickSand regular">Regular</p>
+    <p class="quickSand medium">Medium</p>
+    <p class="quickSand semiBold">Semi-Bold</p>
+    <p class="quickSand bold">Bold</p>
+    </div>
+
   @endwhile
 @endsection

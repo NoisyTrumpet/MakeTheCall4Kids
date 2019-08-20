55f9f63d8732ca7ff22119e38ea26bab5752cbed
CesarBenavides777 <cesarbenavides@icloud.com>
2019-08-20 11:43:53 -0500

    Woocommerce template implementations

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -5,6 +5,10 @@
       @if (has_nav_menu('primary_navigation'))
         {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']) !!}
       @endif
+
+      {{-- @if (has_nav_menu('primary_navigation'))
+  {!! wp_nav_menu($primarymenu) !!}
+@endif --}}
     </nav>
   </div>
 </header>
